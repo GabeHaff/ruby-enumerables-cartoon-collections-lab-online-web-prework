1d98e52e2711e841a17b447045baf7293f8d97e8
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-05 16:00:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_array)
+    dwarf_array.each_with_index do |dwarf, index|
+		index += 1
+    puts "/" + index.to_s + ".*" + dwarf + "/"
+end
+end 
+
+
+def summon_captain_planet(array)
+ array = array.map { |item| "#{item.capitalize}!" }
+  array
+end 
+
+def long_planeteer_calls(array)
+array.any? {|call| call.length>4}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+   cheese.each {|item| cheese_types.each {|cheese| return item if item==cheese}}
 end
+
